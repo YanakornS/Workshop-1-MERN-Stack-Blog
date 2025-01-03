80be832b64f6a44892dc176cb8aab8ae8ab3dfed
YanakornS <654259002@webmail.npru.ac.th>
2025-01-03 13:43:43 +0700

    Update

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -1,8 +1,9 @@
+import daisyui from "daisyui";
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [require("daisyui")],
+  plugins: [daisyui],
 };
